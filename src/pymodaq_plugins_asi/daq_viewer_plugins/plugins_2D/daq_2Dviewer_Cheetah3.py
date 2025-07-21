c1248493abb06e76cef5795f52b57218d543f3ec
Adrien Teurtrie <adteurtrie@u2204-teurtrie.cemes.fr>
2025-07-21 14:46:31 +0200

    emulator working example

--- src/pymodaq_plugins_asi/daq_viewer_plugins/plugins_2D/daq_2Dviewer_Cheetah3.py
+++ src/pymodaq_plugins_asi/daq_viewer_plugins/plugins_2D/daq_2Dviewer_Cheetah3.py
@@ -3,9 +3,15 @@ import numpy as np
 from pymodaq_utils.utils import ThreadCommand
 from pymodaq_data.data import DataToExport, Axis
 from pymodaq_gui.parameter import Parameter
-
+from qtpy import QtWidgets, QtCore
+from qtpy.QtCore import QThread
 from pymodaq.control_modules.viewer_utility_classes import DAQ_Viewer_base, comon_parameters, main
 from pymodaq.utils.data import DataFromPlugins
+from pymodaq_utils.logger import set_logger, get_module_name
+
+from pymodaq_plugins_asi.hardware.cheetah3 import Cheetah3
+
+logger = set_logger(get_module_name(__file__))
 # class PythonWrapperOfYourInstrument:
 #     #  TODO Replace this fake class with the import of the real python wrapper of your instrument
 #     pass
@@ -38,21 +44,27 @@ class DAQ_2DViewer_Cheetah3(DAQ_Viewer_base):
     # TODO add your particular attributes here if any
 
     """
+    live_mode_available = True
+    callback_signal = QtCore.Signal()
     params = comon_parameters + [
-        ## TODO for your custom plugin
-        # elements to be added here as dicts in order to control your custom stage
-        ############
+        {'title' : 'Exposure time', 'name' : 'exposure_time', 'type' : 'float', 'value' : 0.5},
+        {'title' : 'Display image', 'name' : 'is_image', 'type' : 'bool', 'value' : True},
+        {'title' : 'X full binning', 'name' : 'binned_x', 'type' : 'bool', 'value' : False},
+        {'title' : 'Y full binning', 'name' : 'binned_y', 'type' : 'bool', 'value' : False}    
     ]
 
     def ini_attributes(self):
         #  TODO declare the type of the wrapper (and assign it to self.controller) you're going to use for easy
         #  autocompletion
-        self.controller: PythonWrapperOfYourInstrument = None
+        self.controller: Cheetah3 = None
 
         # TODO declare here attributes you want/need to init with a default value
 
         self.x_axis = None
         self.y_axis = None
+        self.is_image = True
+        self.binned_x = False
+        self.binned_y = False
 
     def commit_settings(self, param: Parameter):
         """Apply the consequences of a change of value in the detector settings
@@ -63,8 +75,14 @@ class DAQ_2DViewer_Cheetah3(DAQ_Viewer_base):
             A given parameter (within detector_settings) whose value has been changed by the user
         """
         # TODO for your custom plugin
-        if param.name() == "a_parameter_you've_added_in_self.params":
-            self.controller.your_method_to_apply_this_param_change()
+        if param.name() == "exposure_time":
+            self.controller.exposure_time = param.value()
+        elif param.name() == 'is_image' : 
+            self.is_image = param.value()
+        elif param.name() == 'binned_x' : 
+            self.binned_x = param.value()
+        elif param.name() == 'binned_y' : 
+            self.binned_y = param.value()
         #elif ...
 
     def ini_detector(self, controller=None):
@@ -82,31 +100,26 @@ class DAQ_2DViewer_Cheetah3(DAQ_Viewer_base):
         initialized: bool
             False if initialization failed otherwise True
         """
-        raise NotImplementedError  # TODO when writing your own plugin remove this line and modify the one below
+        # self.controller = self.ini_controller_init(slave_controller = controller, new_controller = Cheetah3() )  # TODO when writing your own plugin remove this line and modify the one below
         if self.is_master:
-            self.controller = PythonWrapperOfYourInstrument()  #instantiate you driver with whatever arguments are needed
-            self.controller.open_communication() # call eventual methods
-            initialized = self.controller.a_method_or_atttribute_to_check_if_init()  # TODO
+            self.controller = Cheetah3()  #instantiate you driver with whatever arguments are needed
+            initialized = self.controller.check_connection()  # TODO
+            info = "The DAQ_viewer Cheetah3 has successfully started"
+            self.x_axis = Axis(data=np.linspace(0,  512 - 1, 512, dtype=int), label='Pixels', index=1)
+            self.y_axis = Axis(data=np.linspace(0, 512 - 1, 512, dtype=int), label='Pixels', index=1)
+
+            self.callback = MyCallback(self.controller.wait_for_acq)
+            self.callback_thread = QtCore.QThread()
+            self.callback.moveToThread(self.callback_thread)
+            self.callback.data_sig.connect(self.emit_data)  # when the wait for acquisition returns (with data taken), emit_data will be fired
+
+            self.callback_signal.connect(self.callback.read_status)
+            self.callback_thread.callback = self.callback
+            self.callback_thread.start()
         else:
             self.controller = controller
             initialized = True
 
-        ## TODO for your custom plugin
-        # get the x_axis (you may want to to this also in the commit settings if x_axis may have changed
-        data_x_axis = self.controller.your_method_to_get_the_x_axis()  # if possible
-        self.x_axis = Axis(data=data_x_axis, label='', units='', index=1)
-
-        # get the y_axis (you may want to to this also in the commit settings if y_axis may have changed
-        data_y_axis = self.controller.your_method_to_get_the_y_axis()  # if possible
-        self.y_axis = Axis(data=data_y_axis, label='', units='', index=0)
-
-        ## TODO for your custom plugin. Initialize viewers pannel with the future type of data
-        self.dte_signal_temp.emit(DataToExport('myplugin',
-                                               data=[DataFromPlugins(name='Mock1', data=["2D numpy array"],
-                                                                     dim='Data2D', labels=['dat0'],
-                                                                     axes=[self.x_axis, self.y_axis]), ]))
-
-        info = "Whatever info you want to log"
         return info, initialized
 
     def close(self):
@@ -116,6 +129,42 @@ class DAQ_2DViewer_Cheetah3(DAQ_Viewer_base):
         if self.is_master:
             #  self.controller.your_method_to_terminate_the_communication()  # when writing your own plugin replace this line
             ...
+
+    def emit_data(self):
+        # Add a bool as arg so that I can pick finishing acquisition or current
+        # Avant de broadcaster les données, il vaut mieux créer une copie pour éviter d'avoir des soucis de pointeur. Le reshape doit faire une copie à priori.
+        """
+            Fonction used to emit data obtained by callback.
+
+            See Also
+            --------
+            daq_utils.ThreadCommand
+        """
+        try:
+            for i in range(self.controller.ntriggers) : 
+                image = self.controller.preview().reshape((512,512)).astype(float) 
+                dtp =[]
+                if self.is_image : 
+                    dtp.append(DataFromPlugins(name='Cheetah3 image',
+                                                data=[np.atleast_1d(
+                                                image) ]))
+                if self.binned_x : 
+                    dtp.append(DataFromPlugins(name = 'Cheetah3 sum X',
+                                    data = [np.atleast_1d(image.sum(axis = 0))],
+                                    dim = 'Data1D'))
+                    
+                if self.binned_y : 
+                    dtp.append(DataFromPlugins(name = 'Cheetah3 sum Y',
+                                    data = [np.atleast_1d(image.sum(axis = 1))],
+                                    dim = 'Data1D'))
+                        
+                self.dte_signal.emit(DataToExport('Cheetah3',
+                                                data=dtp))
+                QtWidgets.QApplication.processEvents()  # here to be sure the timeevents are executed even if in continuous grab mode
+            # self.callback_signal.emit()
+        except Exception as e:
+            print("An exception occured in emit data")
+            self.emit_status(ThreadCommand('Update_Status', [str(e), 'log']))
 
     def grab_data(self, Naverage=1, **kwargs):
         """Start a grab from the detector
@@ -131,34 +180,52 @@ class DAQ_2DViewer_Cheetah3(DAQ_Viewer_base):
         ## TODO for your custom plugin: you should choose EITHER the synchrone or the asynchrone version following
 
         ##synchrone version (blocking function)
-        data_tot = self.controller.your_method_to_start_a_grab_snap()
-        self.dte_signal.emit(DataToExport('myplugin',
-                                          data=[DataFromPlugins(name='Mock1', data=data_tot,
-                                                                dim='Data2D', labels=['label1'],
-                                                                x_axis=self.x_axis,
-                                                                y_axis=self.y_axis), ]))
+        try:
 
-        ##asynchrone version (non-blocking function with callback)
-        self.controller.your_method_to_start_a_grab_snap(self.callback)
+            if kwargs.get('live',False) == True :
+                self.controller.ntriggers = int(2e9)
+                self.controller.start()
+                self.callback_signal.emit()
+
+            else:
+                self.controller.ntriggers = 1
+                self.controller.start()
+                self.callback_signal.emit()  # will trigger the waitfor acquisition
+
+
+        except Exception as e:
+            self.emit_status(ThreadCommand('Update_Status', [str(e), "log"]))
         #########################################################
 
-    def callback(self):
-        """optional asynchrone method called when the detector has finished its acquisition of data"""
-        data_tot = self.controller.your_method_to_get_data_from_buffer()
-        self.dte_signal.emit(DataToExport('myplugin',
-                                          data=[DataFromPlugins(name='Mock1', data=data_tot,
-                                                                dim='Data2D', labels=['label1'],
-                                                                x_axis=self.x_axis,
-                                                                y_axis=self.y_axis), ]))
     def stop(self):
         """Stop the current grab hardware wise if necessary"""
         ## TODO for your custom plugin
-        raise NotImplementedError  # when writing your own plugin remove this line
-        self.controller.your_method_to_stop_acquisition()  # when writing your own plugin replace this line
-        self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
-        ##############################
-        return ''
+        self.controller.stop()
 
+
+class MyCallback(QtCore.QObject):
+    """
+
+    """
+    data_sig = QtCore.Signal()
+    # bool dans le data sig pour gérer le data_sig_temp
+
+    def __init__(self, status_fn):
+        super(MyCallback, self).__init__()
+        self.status_fn = status_fn
+
+    def read_status(self):
+        ind = self.status_fn()
+        if ind == 0 :
+            logger.info('End of acquistion')
+            pass
+        elif ind == 1 :
+            self.data_sig.emit()
+            # faire 2 cas, soit l'acqusition d'1 frame est en cours : data_sig_temp
+            # soit il a fini et il faut data_sig
+
+        else : 
+            raise NotImplementedError('Message to clarify TODO')
 
 if __name__ == '__main__':
     main(__file__)
--- src/pymodaq_plugins_asi/hardware/cheetah3.py
+++ src/pymodaq_plugins_asi/hardware/cheetah3.py
@@ -4,6 +4,18 @@ import requests
 import json
 from pymodaq_plugins_asi.utils import Config
 import re
+from pymodaq_utils.logger import set_logger, get_module_name
+from pymodaq_plugins_asi.hardware import cheetah3_consts as c3c
+from pint import Quantity
+from pathlib import Path
+from PIL import Image
+from io import BytesIO
+import numpy as np
+import socket
+import threading
+
+logger = set_logger(get_module_name(__file__))
+destinations_path = Path(__file__).parent / Path('destinations.json')
 
 class Cheetah3Config :
 
@@ -19,7 +31,7 @@ class Cheetah3Config :
         for file in all_files : 
             m = re.match(r"(.*)(\." + extension + r")", file)
             if m : 
-                selected_files.append(file)
+                selected_files.append(pathlib.Path(folder_path) / pathlib.Path(file))
 
         return selected_files
 
@@ -28,6 +40,13 @@ class Cheetah3() :
     def __init__(self):
         self.config = Cheetah3Config()
         self.serverurl = self.config.config['CHEETAH3']['connection']['serverurl']
+        self.dashboard = self.get_dashboard()
+        self.detector_config = self.get_detector_config()
+        self._bpc_file = None
+        self._dacs_file = None
+        self._exposure_time = Quantity('10ms')
+        self._readout_time = Quantity('10ms')
+        self._ntriggers = 1
 
     def get_request(self, url, expected_status=200):
         response = requests.get(url=url)
@@ -35,7 +54,6 @@ class Cheetah3() :
             raise Exception("Failed GET request: {}, response: {} {}".format(url, response.status_code, response.text))
 
         return response
-
 
     def put_request(self, url, data, expected_status=200):
         response = requests.put(url=url, data=data)
@@ -53,3 +71,186 @@ class Cheetah3() :
 
         # Response "200" is expected when request has succeeded
         self.get_request(url=self.serverurl, expected_status=200)
+        logger.info('Connection to the Cheetah3 successful.')
+        return True
+
+    def get_dashboard(self) : 
+        response = self.get_request(url=self.serverurl + '/dashboard')
+        data = response.text
+        dashboard = json.loads(data)
+        return dashboard
+
+    def get_detector_config(self) : 
+        response = self.get_request(url=self.serverurl + '/detector/config')
+        data = response.text
+        detector_config = json.loads(data)
+        return detector_config
+
+    def load_pixel_config(self):
+        """Load detector parameters required for operation, prints statuses
+        """
+        # load a binary pixel configuration exported by SoPhy, the file should exist on the server
+        response = self.get_request(url=self.serverurl + '/config/load?format=pixelconfig&file=' + self.bpc_file)
+        data = response.text
+        logger.info(f'Response of loading binary pixel configuration file:{data}' )
+
+        #  .... and the corresponding DACs file
+        response = self.get_request(url=self.serverurl + '/config/load?format=dacs&file=' + self.dacs_file)
+        data = response.text
+        logger.info(f'Response of loading DACs file: {data}')
+
+    def set_detector_config(self,trigger_mode = 'continuous',ntriggers=1,trigger_period=0.5) : 
+        if trigger_mode == 'continuous' : 
+            self.set_continuous_mode()
+        elif trigger_mode == 'automatic' : 
+            self.set_automatic_mode(ntriggers = ntriggers)
+        self.put_request(url=self.serverurl +'/detector/config', data = json.dumps(self.detector_config))
+        print(self.detector_config)
+        # logger.info(f'Loaded config :\nTriggerMode : {self.detector_config['TriggerMode']}\nExposureTime : {self.detector_config['ExposureTime']}\nTriggerPeriod : {self.detector_config['TriggerPeriod']}\nnTriggers : {self.detector_config['nTriggers']}')
+
+    def set_continuous_mode(self, **kwargs) : 
+        self.detector_config['TriggerMode'] = 'CONTINUOUS'
+        self.detector_config['TriggerPeriod'] = self.exposure_time.magnitude
+        self.detector_config['ExposureTime'] = self.exposure_time.magnitude
+        self.detector_config['nTriggers'] = 1 
+
+    def set_automatic_mode(self, **kwargs) : 
+        self.detector_config['TriggerMode'] = 'AUTOTRIGSTART_TIMERSTOP'
+        self.detector_config['TriggerPeriod'] = (self.exposure_time + self._readout_time ).magnitude
+        self.detector_config['ExposureTime'] = self.exposure_time.magnitude
+        self.detector_config['nTriggers'] = kwargs['ntriggers'] 
+
+    def set_destination(self, profile : str) : 
+        with open(destinations_path, 'r') as f : 
+            profiles = json.load(f)
+        current_profile = profiles[profile]
+        self.put_request(url = self.serverurl + '/server/destination', data = json.dumps(current_profile))  
+
+    def start(self):
+        """Perform acquisition
+
+        Keyword arguments:
+        serverurl -- the URL of the running SERVAL (string)
+        """
+        self.set_detector_config(ntriggers=self.ntriggers, trigger_mode='automatic')
+        self.set_destination(profile='basic')
+        response = self.get_request(url=self.serverurl + '/measurement/start')
+        data = response.text
+        logger.info('Response of acquisition start: ' + data)
+
+    def preview(self):
+        """Preview of collected data
+
+        Keyword arguments:
+        serverurl -- the URL of the running SERVAL (string)
+        ntrig -- number of triggers to be executed (integer, defaul value is 1)
+        """
+
+        # Getting preview data. This is blocking, so it will wait until an image is ready.
+
+        response = self.get_request(url=self.serverurl + '/measurement/image')
+        image = Image.open(BytesIO(response.content))
+        # Show the data in the image
+        return np.array(image)
+    
+    def get_status(self) :
+        if self.get_dashboard()["Measurement"] is None : 
+            return None
+        else : 
+            return self.get_dashboard()["Measurement"]["Status"]
+
+    def wait_for_acq(self) : 
+        while True : 
+            status = self.get_status() 
+            if status == "DA_RECORDING" : 
+                return 1
+            elif status == "DA_IDLE" : 
+                pass
+            elif status == "DA_PREPARING" :
+                pass
+            elif status == "DA_STOPPING" : 
+                return 0 
+
+    def stop(self) : 
+        response = self.get_request(url=self.serverurl + '/measurement/stop')
+        data = response.text
+        logger.info('Response of acquisition stop : ' + data)
+            
+    @property
+    def bpc_file(self) : 
+        if self._bpc_file is None : 
+            self._bpc_file = self.config.bpcs[0]
+        return self._bpc_file
+
+    @bpc_file.setter
+    def bpc_file(self, filename) : 
+        if filename in self.config.bpcs :
+            self._bpc_file = filename
+        else :
+            logger.info(f'the bpc file : {filename} is not part of the available files.')  
+
+    @property
+    def dacs_file(self) : 
+        if self._dacs_file is None : 
+            self._dacs_file = self.config.dacss[0]
+        return self._dacs_file
+
+    @dacs_file.setter
+    def dacs_file(self, filename) : 
+        if filename in self.config.dacss :
+            self._dacs_file = filename
+        else :
+            logger.info(f'the dacs file : {filename} is not part of the available files.') 
+
+    @property
+    def exposure_time (self) : 
+        return self._exposure_time.to('s')
+    
+    @exposure_time.setter
+    def exposure_time(self,value) : 
+        q = Quantity(value,'s')
+        self._exposure_time = q.to('s') 
+
+    @property
+    def ntriggers(self) : 
+        return self._ntriggers
+
+    @ntriggers.setter
+    def ntriggers(self, value) : 
+        self._ntriggers = value
+    
+
+
+
+if __name__ == '__main__' : 
+    cam = Cheetah3()
+    cam.check_connection()
+    # cam.start_listening()
+    # print(cam.get_dashboard())
+    # print(cam.bpc_file)
+    # print(cam.detector_config)
+    # print(cam.serverurl)
+    cam.exposure_time = '0.5s'
+    cam.set_detector_config(trigger_mode='automatic',ntriggers=75)
+    # print(cam.get_detector_config())
+    cam.set_destination("basic")
+    # response = cam.get_request(url = cam.serverurl + '/server/destination')
+    # data = response.text
+    # # print(data)
+    cam.start()
+    # print(cam.get_dashboard())
+    cam.wait_for_acq()
+    i = 0 
+    measure = True
+    while measure :
+        try :  
+            print(cam.preview().sum() + i)
+            i+=1
+            if cam.get_status() ==  'DA_IDLE' : 
+                measure= False
+        except KeyboardInterrupt : 
+            cam.stop()
+            break
+    #     cam.wait_for_acq()
+        # cam.wait_for_acq()
+    # print(cam.preview().shape)
